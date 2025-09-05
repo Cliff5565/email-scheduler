05dcb394d4f75d988a54ab662350361e40ac514a
Cliff5565 <fishermancliff@yahoo.com.hk>
2025-09-06 00:32:51 +0800

    Add files via upload

---
+++ emailWorker.js
@@ -0,0 +1,47 @@
+// workers/// workers/emailWorker.js
+import nodemailer from "nodemailer";
+import { Worker } from "bullmq";
+import mongoose from "mongoose";
+import Email from "../models/Email.js";
+
+mongoose.connect(process.env.MONGO_URI);
+
+const worker = new Worker("emails", async job => {
+  const { to, subject, message, emailId } = job.data;
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  await transporter.sendMail({ from: process.env.EMAIL_USER, to, subject, text: message });
+
+  await Email.findByIdAndUpdate(emailId, { status: "SENT", sentAt: new Date() });
+  console.log(Email sent to ${to}`);
+});.js
+import nodemailer from "nodemailer";
+import { Worker } from "bullmq";
+import mongoose from "mongoose";
+import Email from "../models/Email.js";
+
+mongoose.connect(process.env.MONGO_URI);
+
+const worker = new Worker("emails", async job => {
+  const { to, subject, message, emailId } = job.data;
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  await transporter.sendMail({ from: process.env.EMAIL_USER, to, subject, text: message });
+
+  await Email.findByIdAndUpdate(emailId, { status: "SENT", sentAt: new Date() });
+  console.log( Email sent to ${to}`);
+});

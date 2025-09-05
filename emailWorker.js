1523816fad7a3cbff5a1fca07523056680f1b605
Cliff5565 <fishermancliff@yahoo.com.hk>
2025-09-06 00:33:26 +0800

    Update emailWorker.js

--- emailWorker.js
+++ emailWorker.js
@@ -1,4 +1,4 @@
-// workers/// workers/emailWorker.js
+// workers/emailWorker.js
 import nodemailer from "nodemailer";
 import { Worker } from "bullmq";
 import mongoose from "mongoose";
@@ -20,28 +20,5 @@ const worker = new Worker("emails", async job => {
   await transporter.sendMail({ from: process.env.EMAIL_USER, to, subject, text: message });
 
   await Email.findByIdAndUpdate(emailId, { status: "SENT", sentAt: new Date() });
-  console.log(Email sent to ${to}`);
-});.js
-import nodemailer from "nodemailer";
-import { Worker } from "bullmq";
-import mongoose from "mongoose";
-import Email from "../models/Email.js";
-
-mongoose.connect(process.env.MONGO_URI);
-
-const worker = new Worker("emails", async job => {
-  const { to, subject, message, emailId } = job.data;
-
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  await transporter.sendMail({ from: process.env.EMAIL_USER, to, subject, text: message });
-
-  await Email.findByIdAndUpdate(emailId, { status: "SENT", sentAt: new Date() });
-  console.log( Email sent to ${to}`);
+  console.log(`✅ Email sent to ${to}`);
 });
